21eff5a0d261e3e9338710722cf1b04174b821e0
Mike Kravtsov <mike4040@me.com>
2019-11-21 09:14:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const shakeWord = word => {
   const letters = word.split('');
-  const firts = letters.shift();
   const last = letters.pop();
 
-  return [firts, ...letters.sort(() => Math.random() - 0.5), last].join('');
+  return [letters.shift(), ...letters.sort(() => Math.random() - 0.5), last].join('');
 }
 
-const shake = sentence => sentence.split(' ').map(shakeWord).join(' ');
+const shuffle = sentence => sentence.split(' ').map(shakeWord).join(' ');
 
 for (let i = 0; i <= 5; i++)
-  console.log(shake('This is because the human mind does not read every letter by itself but the word as a whole'));
+  console.log(shuffle('This is because the human mind does not read every letter by itself but the word as a whole'));
