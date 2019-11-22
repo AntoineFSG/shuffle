fab90363c812860827bb0cb6540c974f9d5d6321
Antoine FSG <35838884+AntoineFSG@users.noreply.github.com>
2019-11-21 22:02:03 -0800

    a new approach with splice
    Scramble the 4-letters words.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const shakeWord = word => {
-  const letters = word.split('');
-  const last = letters.pop();
+const scrambleWordsWithSplice = sentence => {
+  //breaking down the sentence in an array of arrays of letters and shuffling the letters in each array
+  const shuffledArraysOfLetters = sentence
+    .split(" ")
+    .map(word => {
+      return word.split("");
+    })
+    .map(arr => {
+      let arrTemp = [...arr];
+      for (let i = 0; i < arr.length - 3; i += 2) {
+        arrTemp.splice(i + 1, 2, arr[i + 2], arr[i + 1]);
+      }
+      console.log(arrTemp);
+      //replacing the original array with its shuffled copy
+      return arrTemp;
+    });
+  //going back to a sentence
+  const shuffledSentence = shuffledArraysOfLetters
+    .map(letters => {
+      return letters.join("");
+    })
+    .join(" ");
+  return shuffledSentence;
+};
 
-  return [letters.shift(), ...letters.sort(() => Math.random() - 0.5), last].join('');
-}
-
-const shuffle = sentence => sentence.split(' ').map(shakeWord).join(' ');
-
-for (let i = 0; i <= 5; i++)
-  console.log(shuffle('This is because the human mind does not read every letter by itself but the word as a whole'));
+console.log(
+  scrambleWordsWithSplice(
+    "This is because the human mind does not read every letter by itself but the word as a whole, it works for absolutely any sentence !"
+  )
+);
